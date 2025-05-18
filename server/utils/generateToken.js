cdf6ab987bf9bfb01eb038d0d316bf0ddc425e01
mohan-bee <mohn08052006@gmail.com>
2025-05-18 20:21:23 +0530

    cookie configured

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -6,6 +6,6 @@ exports.generateToken = (user, res) => {
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true,
             sameSite: 'none',
-            secure: process.env.NODE_ENV === "production",
+            secure: true,
         })
 }
