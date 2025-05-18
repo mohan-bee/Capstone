db2414738cb6e5c53c4b90a935712c5012fab543
mohan-bee <mohn08052006@gmail.com>
2025-05-18 20:28:08 +0530

    cookie configured

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -4,8 +4,5 @@ exports.generateToken = (user, res) => {
     const token = jwt.sign({userId: user._id, username: user.username}, process.env.JWT_SECRET, {expiresIn:'7d'})
     res.cookie("token", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-        secure: process.env.NODE_ENV === 'production',
     })
 }
